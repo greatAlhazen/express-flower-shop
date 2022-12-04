84d328264bb2ed5757a11e8c586beeb6302f423f
EnesUraz <axeprime91@gmail.com>
2022-12-04 12:22:16 +0300

    create responsive navbar

---
+++ public/scripts/navbar.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('#menu');
+const allLinks = document.querySelectorAll('#links');
+const navLinks = document.querySelector('.navList');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('fa-times');
+    navLinks.classList.toggle('active');
+});
+
+const activateLink = (value) =>{
+    allLinks.forEach((item) =>{
+        item.classList.remove('active');
+        value.classList.add('active');
+    })
+}
+
+allLinks.forEach((item) =>{
+    item.addEventListener('click',function(){
+        activateLink(item)
+    })
+})
