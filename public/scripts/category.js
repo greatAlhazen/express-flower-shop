216c26eb2667b91c3d15080bb295087443f2c178
EnesUraz <axeprime91@gmail.com>
2022-12-06 12:25:15 +0300

    create categories section

---
+++ public/scripts/category.js
@@ -0,0 +1,13 @@
+const category = document.querySelectorAll('.category');
+
+category.forEach((item) =>{
+    item.addEventListener('mouseover',function(){
+        if(item.classList.contains('category-1')){
+            item.firstElementChild.src = '/images/annuals/marigoldCat.jpg';
+        }else if(item.classList.contains('category-2')){
+            item.firstElementChild.src = '/images/biennials/hollyhockCat.jpg';
+        }else{
+            item.firstElementChild.src = '/images/perennials/lavenderCat.jpg';
+        }
+    })
+})
