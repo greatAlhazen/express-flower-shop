722a219bacf318763bba9de952e40e2d1a0f59d1
EnesUraz <axeprime91@gmail.com>
2022-12-18 12:48:56 +0300

    create admin dashboard and add some ui  designs

---
+++ public/scripts/dashboard.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#adminNavbarMenu');
+const sidebar = document.querySelector('.adminSidebar');
+const navbar = document.querySelector('.adminNavbarActions');
+const logout = document.querySelector('.adminNavbarLogout');
+
+menu.onclick = () =>{
+    sidebar.classList.toggle('active');
+    menu.classList.toggle('fa-times');
+}
+
+window.addEventListener('scroll',function(){
+    if(this.window.scrollY >= 20){
+        navbar.classList.add('active')
+        logout.classList.add('active');
+    }else{
+        navbar.classList.remove('active');
+        logout.classList.remove('active');
+    }
+})
